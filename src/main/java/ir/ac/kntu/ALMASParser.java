04b2d4a4851f010ae631c4ee8be6729d1e289d4c
shayandaneshvar <daneshvarshayan@gmail.com>
2021-12-16 14:21:40 +0330

    update grammar and files/ fix ambiguity and empty string matching warning

--- src/main/java/ir/ac/kntu/ALMASParser.java
+++ src/main/java/ir/ac/kntu/ALMASParser.java
@@ -17,20 +17,19 @@ public class ALMASParser extends Parser {
 		new PredictionContextCache();
 	public static final int
 		PROGRAM_BODY=1, ASSIGNMENT=2, STRING_ASSIGNMENT=3, DECIMAL_ASSIGNMENT=4, 
-		BOOLEAN_ASSIGNMENT=5, IF=6, ELIF=7, ELSE=8, EXPRESSIONS=9, EXPRESSION=10, 
-		RELOP_EXPRESSION=11, STATEMENTS=12, LOOP=13, LOOP_STATEMENTS=14, WS=15, 
-		PRIMITIVE_TYPE=16, RETURN_TYPE=17, FUNCTION=18, FUNCTION_CALL=19, FUNC_INPUTS=20, 
-		MORE_FUNC_INPUTS=21, RETURN_ST=22, FUNC_ARGS=23, MORE_FUNC_ARGS=24, BOOLEAN_CAST=25, 
-		DECIMAL_CAST=26, STRING_CAST=27, IDENTIFIER=28, DECIMAL_TYPE=29, STRING_TYPE=30, 
-		BOOLEAN_TYPE=31, VOID_TYPE=32, ASSIGNMENT_OP=33, RELATIONAL_OPERATOR=34, 
-		MUL=35, DIV=36, ADD=37, SUB=38, MOD=39, INTEGER_DIV=40, AND=41, OR=42, 
-		XOR=43, NOT=44, UNARY_OP=45, BINARY_BOOLEAN_OP=46, EQUAL=47, COMMA=48, 
-		COLON=49, CURLY_RIGHT=50, CURLY_LEFT=51, LEFT_P=52, RIGHT_P=53, PRINT_OP=54, 
-		INPUT_OP=55, BOOLEAN=56, IF_SYMBOL=57, ELSE_IF_SYMBOL=58, ELSE_SYMBOL=59, 
-		FOR_SYMBOL=60, FUNCTION_SYMBOL=61, RETURN_SYMBOL=62, CONTINUE_SYMBOL=63, 
-		BREAK_SYMBOL=64, SIGN=65, NUMBER=66, DECIMAL=67, OPTIONAL_DECIMAL=68, 
-		STRING_LITERAL=69, STRING=70, ANYCHAR=71, BOOLEAN_CAST_SYMBOL=72, STRING_CAST_SYMBOL=73, 
-		DECIMAL_CAST_SYMBOL=74;
+		BOOLEAN_ASSIGNMENT=5, IF=6, BARE_IF=7, ELIF=8, BARE_ELIF=9, ELSE=10, EXPRESSIONS=11, 
+		EXPRESSION=12, RELOP_EXPRESSION=13, STATEMENTS=14, LOOP=15, LOOP_STATEMENTS=16, 
+		WS=17, PRIMITIVE_TYPE=18, RETURN_TYPE=19, FUNCTION=20, FUNCTION_CALL=21, 
+		FUNC_INPUTS=22, MORE_FUNC_INPUTS=23, RETURN_ST=24, FUNC_ARGS=25, MORE_FUNC_ARGS=26, 
+		BOOLEAN_CAST=27, DECIMAL_CAST=28, STRING_CAST=29, IDENTIFIER=30, DECIMAL_TYPE=31, 
+		STRING_TYPE=32, BOOLEAN_TYPE=33, VOID_TYPE=34, ASSIGNMENT_OP=35, RELATIONAL_OPERATOR=36, 
+		MUL=37, DIV=38, ADD=39, SUB=40, MOD=41, INTEGER_DIV=42, AND=43, OR=44, 
+		XOR=45, NOT=46, UNARY_OP=47, BINARY_BOOLEAN_OP=48, EQUAL=49, COMMA=50, 
+		COLON=51, CURLY_RIGHT=52, CURLY_LEFT=53, LEFT_P=54, RIGHT_P=55, PRINT_OP=56, 
+		INPUT_OP=57, BOOLEAN=58, IF_SYMBOL=59, ELSE_IF_SYMBOL=60, ELSE_SYMBOL=61, 
+		FOR_SYMBOL=62, FUNCTION_SYMBOL=63, RETURN_SYMBOL=64, CONTINUE_SYMBOL=65, 
+		BREAK_SYMBOL=66, SIGN=67, NUMBER=68, DECIMAL=69, STRING_LITERAL=70, STRING=71, 
+		ANYCHAR=72, BOOLEAN_CAST_SYMBOL=73, STRING_CAST_SYMBOL=74, DECIMAL_CAST_SYMBOL=75;
 	public static final int
 		RULE_program = 0;
 	private static String[] makeRuleNames() {
@@ -44,30 +43,31 @@ public class ALMASParser extends Parser {
 		return new String[] {
 			null, null, null, null, null, null, null, null, null, null, null, null, 
 			null, null, null, null, null, null, null, null, null, null, null, null, 
-			null, null, null, null, null, "'#'", "'$'", "'?'", "'_'", "'='", null, 
-			"'*'", "'/'", "'+'", "'-'", "'%'", "'//'", "'&'", "'|'", "'^'", "'!'", 
-			null, null, "'is'", "','", "':'", "'{'", "'}'", "'('", "')'", "'@print'", 
-			"'@input'", null, "'if'", "'elif'", "'else'", "'loop'", "'fn'", "'ret'", 
-			"'go'", "'break'", null, null, null, null, "'\"'", null, null, "'@?'", 
-			"'@$'", "'@#'"
+			null, null, null, null, null, null, null, "'#'", "'$'", "'?'", "'_'", 
+			"'='", null, "'*'", "'/'", "'+'", "'-'", "'%'", "'//'", "'&'", "'|'", 
+			"'^'", "'!'", null, null, "'is'", "','", "':'", "'{'", "'}'", "'('", 
+			"')'", "'@print'", "'@input'", null, "'if'", "'elif'", "'else'", "'loop'", 
+			"'fn'", "'ret'", "'go'", "'break'", null, null, null, "'\"'", null, null, 
+			"'@?'", "'@$'", "'@#'"
 		};
 	}
 	private static final String[] _LITERAL_NAMES = makeLiteralNames();
 	private static String[] makeSymbolicNames() {
 		return new String[] {
 			null, "PROGRAM_BODY", "ASSIGNMENT", "STRING_ASSIGNMENT", "DECIMAL_ASSIGNMENT", 
-			"BOOLEAN_ASSIGNMENT", "IF", "ELIF", "ELSE", "EXPRESSIONS", "EXPRESSION", 
-			"RELOP_EXPRESSION", "STATEMENTS", "LOOP", "LOOP_STATEMENTS", "WS", "PRIMITIVE_TYPE", 
-			"RETURN_TYPE", "FUNCTION", "FUNCTION_CALL", "FUNC_INPUTS", "MORE_FUNC_INPUTS", 
-			"RETURN_ST", "FUNC_ARGS", "MORE_FUNC_ARGS", "BOOLEAN_CAST", "DECIMAL_CAST", 
-			"STRING_CAST", "IDENTIFIER", "DECIMAL_TYPE", "STRING_TYPE", "BOOLEAN_TYPE", 
-			"VOID_TYPE", "ASSIGNMENT_OP", "RELATIONAL_OPERATOR", "MUL", "DIV", "ADD", 
-			"SUB", "MOD", "INTEGER_DIV", "AND", "OR", "XOR", "NOT", "UNARY_OP", "BINARY_BOOLEAN_OP", 
-			"EQUAL", "COMMA", "COLON", "CURLY_RIGHT", "CURLY_LEFT", "LEFT_P", "RIGHT_P", 
-			"PRINT_OP", "INPUT_OP", "BOOLEAN", "IF_SYMBOL", "ELSE_IF_SYMBOL", "ELSE_SYMBOL", 
-			"FOR_SYMBOL", "FUNCTION_SYMBOL", "RETURN_SYMBOL", "CONTINUE_SYMBOL", 
-			"BREAK_SYMBOL", "SIGN", "NUMBER", "DECIMAL", "OPTIONAL_DECIMAL", "STRING_LITERAL", 
-			"STRING", "ANYCHAR", "BOOLEAN_CAST_SYMBOL", "STRING_CAST_SYMBOL", "DECIMAL_CAST_SYMBOL"
+			"BOOLEAN_ASSIGNMENT", "IF", "BARE_IF", "ELIF", "BARE_ELIF", "ELSE", "EXPRESSIONS", 
+			"EXPRESSION", "RELOP_EXPRESSION", "STATEMENTS", "LOOP", "LOOP_STATEMENTS", 
+			"WS", "PRIMITIVE_TYPE", "RETURN_TYPE", "FUNCTION", "FUNCTION_CALL", "FUNC_INPUTS", 
+			"MORE_FUNC_INPUTS", "RETURN_ST", "FUNC_ARGS", "MORE_FUNC_ARGS", "BOOLEAN_CAST", 
+			"DECIMAL_CAST", "STRING_CAST", "IDENTIFIER", "DECIMAL_TYPE", "STRING_TYPE", 
+			"BOOLEAN_TYPE", "VOID_TYPE", "ASSIGNMENT_OP", "RELATIONAL_OPERATOR", 
+			"MUL", "DIV", "ADD", "SUB", "MOD", "INTEGER_DIV", "AND", "OR", "XOR", 
+			"NOT", "UNARY_OP", "BINARY_BOOLEAN_OP", "EQUAL", "COMMA", "COLON", "CURLY_RIGHT", 
+			"CURLY_LEFT", "LEFT_P", "RIGHT_P", "PRINT_OP", "INPUT_OP", "BOOLEAN", 
+			"IF_SYMBOL", "ELSE_IF_SYMBOL", "ELSE_SYMBOL", "FOR_SYMBOL", "FUNCTION_SYMBOL", 
+			"RETURN_SYMBOL", "CONTINUE_SYMBOL", "BREAK_SYMBOL", "SIGN", "NUMBER", 
+			"DECIMAL", "STRING_LITERAL", "STRING", "ANYCHAR", "BOOLEAN_CAST_SYMBOL", 
+			"STRING_CAST_SYMBOL", "DECIMAL_CAST_SYMBOL"
 		};
 	}
 	private static final String[] _SYMBOLIC_NAMES = makeSymbolicNames();
@@ -132,11 +132,11 @@ public class ALMASParser extends Parser {
 		@Override public int getRuleIndex() { return RULE_program; }
 		@Override
 		public void enterRule(ParseTreeListener listener) {
-			if ( listener instanceof ALMASListener ) ((ALMASListener)listener).enterProgram(this);
+			if ( listener instanceof ALMASListener) ((ALMASListener)listener).enterProgram(this);
 		}
 		@Override
 		public void exitRule(ParseTreeListener listener) {
-			if ( listener instanceof ALMASListener ) ((ALMASListener)listener).exitProgram(this);
+			if ( listener instanceof ALMASListener) ((ALMASListener)listener).exitProgram(this);
 		}
 	}
 
@@ -175,7 +175,7 @@ public class ALMASParser extends Parser {
 	}
 
 	public static final String _serializedATN =
-		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3L\13\4\2\t\2\3\2\7"+
+		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3M\13\4\2\t\2\3\2\7"+
 		"\2\6\n\2\f\2\16\2\t\13\2\3\2\2\2\3\2\2\2\2\n\2\7\3\2\2\2\4\6\7\3\2\2\5"+
 		"\4\3\2\2\2\6\t\3\2\2\2\7\5\3\2\2\2\7\b\3\2\2\2\b\3\3\2\2\2\t\7\3\2\2\2"+
 		"\3\7";
