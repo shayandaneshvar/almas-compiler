377ccd5cface97b56070408dac86cf2f8a2a4ae8
shayandaneshvar <daneshvarshayan@gmail.com>
2021-12-20 14:20:31 +0330

    add splash

--- src/main/java/ir/ac/kntu/graphics/Editor.java
+++ src/main/java/ir/ac/kntu/graphics/Editor.java
@@ -1,14 +1,18 @@
 package ir.ac.kntu.graphics;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.SceneAntialiasing;
+import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.io.IOException;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -17,7 +21,7 @@ public class Editor extends Application {
     private Scene scene;
     private Stage stage;
 
-    public static Editor getInstance(){
+    public static Editor getInstance() {
         return Optional.of(INSTANCE).get();
     }
 
@@ -31,24 +35,53 @@ public class Editor extends Application {
 
     @Override
     public void init() throws Exception {
+        Platform.runLater(this::showSplash);
         AnchorPane root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/editor.fxml")));
         scene = new Scene(root, 800, 600, false, SceneAntialiasing.BALANCED);
         INSTANCE = this;
     }
 
-    @Override
-    public void start(Stage stage) {
-        this.stage = stage;
+    private void showSplash() {
+        AnchorPane root;
+        try {
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/splash.fxml")));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        Scene scene = new Scene(root, root.getPrefWidth(), root.getPrefHeight(), false, SceneAntialiasing.BALANCED);
+        stage = new Stage();
         stage.setScene(scene);
-        stage.initStyle(StageStyle.DECORATED);
-        stage.setResizable(false);
-        stage.setScene(scene);
-        stage.setTitle("ALMAS IDE with Integrated Compiler");
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.initModality(Modality.NONE);
         stage.show();
+        Platform.runLater(() -> {
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            stage.close();
+        });
+    }
+
+    @Override
+    public void start(Stage stage) throws InterruptedException {
+        this.stage.setOnHiding(e->{
+            System.out.println("Closing Splash..");
+            this.stage = stage;
+            stage.setScene(scene);
+            stage.initStyle(StageStyle.DECORATED);
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.setTitle("ALMAS IDE with Integrated Compiler");
+            stage.show();
 //        stage.setAlwaysOnTop(true);
-        stage.setOnCloseRequest(status -> System.exit(0));
-        stage.getIcons().add(new Image("file://" + Objects.requireNonNull(getClass()
-                .getResource("/images/almas.jfif")).getFile()));
+            stage.setOnCloseRequest(status -> System.exit(0));
+            stage.getIcons().add(new Image("file://" + Objects.requireNonNull(getClass()
+                    .getResource("/images/almas.jfif")).getFile()));
+        });
+
     }
 
 
